4ae8812f6abd60ec334aa86fc104bbe18a97f71a
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-09 07:07:40 +0100

    Create form.js

---
+++ web/web-worker/js-worker/form.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form");
+const input = document.querySelector('input[type="number"]');
+const result = document.querySelector("p#result");
+const worker = new Worker("fibonacci.js");
+
+worker.onmessage = (event) => {
+  result.textContent = event.data;
+  console.log(`Got: ${event.data}`);
+};
+
+worker.onerror = (error) => {
+  console.log(`Worker error: ${error.message}`);
+  throw error;
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  worker.postMessage(input.value);
+  input.value = "";
+};
