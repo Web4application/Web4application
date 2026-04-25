a06895d55d063b12b77dd74862f092bb51338ae3
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-04-26 00:03:16 +0100

    Create sw.js
    Signed-off-by: Seriki yakub <167559384+Web4application@users.noreply.github.com>

---
+++ boot-system/sw.js
@@ -0,0 +1,22 @@
+const CACHE = "kubuhai-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/flutter.js",
+  "/icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
