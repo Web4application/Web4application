21d0be22666e7fbb31b2bce4fa85afecd68a92b8
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-04-26 01:36:51 +0100

    Create file.sh
    Signed-off-by: Seriki yakub <167559384+Web4application@users.noreply.github.com>

---
+++ Web4-OS/file.sh
@@ -0,0 +1,13 @@
+git init
+git remote add origin ...
+git add .
+git commit -m "web4-os init"
+git push
+git init
+git add .
+git commit -m "web4-os initial release"
+git branch -M main
+git remote add origin https://github.com/web4application/web4application/web4-os
+npm start
+npm run build
+git push -u origin main
