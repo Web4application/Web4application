5534f2d2413a5e2fc1d3f4286a7393ea82311db9
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-04-28 08:39:51 +0100

    Update main.js
    Signed-off-by: Seriki yakub <167559384+Web4application@users.noreply.github.com>

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 import "./style.css";
-import web4logo from '/web4.png.jpg';
+import web4logo from '/web4.png';
 
 // Instantiate the SDK
 const discordSdk = new DiscordSDK(import.meta.env.1169709827145089064);
